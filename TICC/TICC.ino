1df25b52c1a46313f6b4adab574ebf4648ea6c2f
Tom-McDermott <tom.n5eg@gmail.com>
2016-07-04 09:07:08 -0700

    Modify in-process debugging code.  This fixes some errors, gets the TDC7200 talking. INT STATUS and Time1 seems to read OK, but other registers not yet returning the correct values expected.

--- TICC/TICC.ino
+++ TICC/TICC.ino
@@ -40,7 +40,7 @@ void setup() {
   for(i = 0; i < ARRAY_SIZE(channels); ++i)
     channels[i].setup();
 
-  attachInterrupt(digitalPinToInterrupt(interruptPin), coarseTimer, RISING);
+//  attachInterrupt(digitalPinToInterrupt(interruptPin), coarseTimer, RISING);
 
   delay(10);
 
@@ -48,17 +48,24 @@ void setup() {
       channels[i].ready_next();
 
   Serial.begin(115200);
+  Serial.println("");
   Serial.println("Starting...");
 }
 
 void loop() {
   int i;
 
-  for(i = 0; i < ARRAY_SIZE(channels); ++i) {
+  delay(5000);
+
+  for(i = 0; i < 1 /*ARRAY_SIZE(channels)*/; ++i) {
     if(channels[i].STOP)
       channels[i].stopTime = coarseCount;
 
     if(channels[i].INTB) {
+
+      Serial.print(" Int Status = ");
+      Serial.println(channels[i].readreg8(INT_STATUS), HEX);
+      
       channels[i].time_interval = channels[i].read();
       channels[i].time_stamp = channels[i].stopTime - channels[i].time_interval;
       channels[i].ready_next();
@@ -68,8 +75,11 @@ void loop() {
       Serial.print(channels[i].time_interval);
       Serial.print(" timestamp: ");
       Serial.println(channels[i].time_stamp);
+
     }
+  
   }
+  Serial.println("");
 }
 
 // ISR for timer. NOTE: uint_64 rollover would take
@@ -93,7 +103,10 @@ tdc7200Channel::tdc7200Channel(char id, int enable, int intb, int csb, int stop)
 // Initial config for TDC7200
 
 void tdc7200Channel::setup() {
-  digitalWrite(ENABLE, HIGH);
+  digitalWrite(ENABLE, LOW);
+  delay(10);
+  digitalWrite(ENABLE, HIGH);  // Needs a low-to-high transition to enable
+  delay(10);
 }
 
 void tdc7200Channel::reset() {
@@ -101,9 +114,10 @@ void tdc7200Channel::reset() {
 	stopTime = 0;
 }
 
-int readReg24(uint8_t address) {
-	int value = 0;
-	SPI.transfer(address);
+unsigned long readReg24(uint8_t address) {
+	unsigned long value = 0;
+
+	SPI.transfer(address & 0x1f);
 
 	//  These values are 24-bit and we're reading them into a 32-bit
 	//  variable.
@@ -111,24 +125,39 @@ int readReg24(uint8_t address) {
 	//  This does some fancy pointer games to read it into the int starting
 	//  at the 2nd byte in rather than the 1st.  That should get the correct
 	//  2-4bit value read.
-	SPI.transfer((uint8_t *) &value + 1, 3);
+
+  int msb = SPI.transfer(0x00);
+  int mid = SPI.transfer(0x00);
+  int lsb = SPI.transfer(0x00);
+
+  value = (msb << 16) + (mid << 8) + lsb;
+  
+//  This clobbers the stack at runtime:
+//	SPI.transfer((uint8_t *) &value + 1, 3);
 
 	return value;
 }
 
 // Read TDC for channel
 long int tdc7200Channel::read() {
-	//  Start a SPI transaction
-	//  Max speed for the tdc7200 is 20MHz
-	//  CPOL = 0; CPHA = 0
-	SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
-	digitalWrite(CSB, LOW);
+  //  Start a SPI transaction
+  //  Max speed for the tdc7200 is 20MHz
+  //  CPOL = 0; CPHA = 0
+  SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
+  digitalWrite(CSB, LOW);
 
-	time1Result = readReg24(TIME1);
+  time1Result = readReg24(TIME1);
   time2Result  = readReg24(TIME2);
-	clock1Result = readReg24(CLOCK_COUNT1);
-	cal1Result = readReg24(CALIBRATION1);
+  clock1Result = readReg24(CLOCK_COUNT1);
+  cal1Result = readReg24(CALIBRATION1);
   cal2Result = readReg24(CALIBRATION2);
+
+  Serial.print("::read time1Result="), Serial.print(time1Result);
+  Serial.print(" time2Result="), Serial.print(time2Result);
+  Serial.print(" clock1Result="), Serial.println(clock1Result);
+  Serial.print(" cal1Result="), Serial.print(cal1Result);
+  Serial.print(" cal2Result="), Serial.println(cal2Result);
+  
   uint32_t calc_time;
   uint16_t tempu16;
   uint32_t tempu32;
@@ -144,17 +173,43 @@ long int tdc7200Channel::read() {
   // Add in another 32bit variable.  Given the limitations on
   // inputs, these two 32 bits still won't overflow.
   tempu32 += clock1Result * CLOCK_PERIOD_PS;
-  return tempu32;
-	digitalWrite(CSB, HIGH);
-	SPI.endTransaction();
+  // return tempu32;
+  digitalWrite(CSB, HIGH);
+  SPI.endTransaction();
 
-  	return (long)tempu32;
+  return (long)tempu32;
 }
 
 // Enable next measurement cycle
 void tdc7200Channel::ready_next() {
     // needs to set the enable bit (START_MEAS in CONFIG1)
-    write(CONFIG1, 0x03);  // measurement mode 2 ('01')
+
+      delay(1);
+
+//      Serial.print("CFG1 CFG2:  = ");
+//      Serial.print(readreg8(CONFIG1), HEX);
+//      Serial.println(readreg8(CONFIG2), HEX);
+
+      
+      write(CONFIG2, 0x80);  // Cal2 is 20 clocks
+
+      // set the stop mask to 2 (need at least 2 ext clocks before
+      // stop gate opens
+ 
+      write(CLOCK_CNTR_STOP_MASK_H, 0x00);
+      write(CLOCK_CNTR_STOP_MASK_L, 0x01);
+
+//      Serial.print("Stop mask POST value = ");
+//      Serial.print(readreg8(CLOCK_CNTR_STOP_MASK_H), HEX);
+//      Serial.print(" ");
+//      Serial.println(readreg8(CLOCK_CNTR_STOP_MASK_L), HEX);
+      
+      delay(1);
+
+      write(INT_STATUS, 0x1f);  //clear interrupts
+
+      write(CONFIG1, 0x83);  // Measurement mode 2
+      //write(CONFIG1, 0x81);  // Measurement mode 1
 }
 
 // Calculate and print time interval to serial
@@ -162,27 +217,32 @@ void output_ti() {
 }
 
 void tdc7200Channel::write(byte address, byte value) {
+
   // take the chip select low to select the device:
-  	SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
-	digitalWrite(CSB, LOW);
+  SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
+  digitalWrite(CSB, LOW);
 
-	SPI.transfer16(address << 8 | value);
+  // Force Address bit 6 to one for a write
+  SPI.transfer16((address | 0x40) << 8 | value);
 
-	digitalWrite(CSB, HIGH);
-	SPI.endTransaction();
+  digitalWrite(CSB, HIGH);
+  SPI.endTransaction();
 }
 
-/* byte readTDC7200(struct chanTDC7200 *channel, byte address) {
+byte tdc7200Channel::readreg8(byte address) {
   byte inByte = 0;
 
+  SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
   // take the chip select low to select the device:
-  digitalWrite(channel->CSB, LOW);
+  digitalWrite(CSB, LOW);
 
-  SPI.transfer(address);
+  SPI.transfer(address & 0x1f);
   inByte = SPI.transfer(0x00);
 
-  digitalWrite(channel->CSB, HIGH);
+  digitalWrite(CSB, HIGH);
+  SPI.endTransaction();
 
   return inByte;
-} */
+}
+
 

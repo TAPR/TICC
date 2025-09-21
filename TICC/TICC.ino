1fda432a593ee28bdaa7d4fabd60b563e8f2b79b
N8UR <jra@febo.com>
2025-09-21 18:56:01 -0400

    Version update

--- TICC/TICC.ino
+++ TICC/TICC.ino
@@ -7,8 +7,8 @@
 // Portions Copyright Jeremy McDermond NH6Z 2016
 // Licensed under BSD 2-clause license
 
-// 16 September 2025 - version 2
-extern const char SW_VERSION[17] = "20250916.1";
+// 21 September 2025 - version 2
+extern const char SW_VERSION[17] = "20250921.1";
 extern const char SW_TAG[6] = "BETA";
 
 /*

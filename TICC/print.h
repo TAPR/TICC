60160e0c4235c2e75bd43f8e4bb6d8246257865c
N8UR <jra@febo.com>
2025-09-30 10:26:03 -0400

    Remove unused test functions
    - Remove test_optimized_print() and test_places_and_wrap() functions
    - Remove corresponding function declarations from print.h
    - These functions were never called and consumed ~74 bytes of RAM
    - Code cleanup with no functional impact

--- TICC/print.h
+++ TICC/print.h
@@ -15,9 +15,6 @@ int print_timestamp(
   bool use_wrap = true
 );
 
-// Test function for verifying the optimized print routine
-void test_optimized_print();
-void test_places_and_wrap();
 
 // Function to update cached config parameters (call when config changes)
 void update_cached_config();
